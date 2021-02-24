8a9e91b10199d4d923f6405f3284849350003bd4
Doron Raifman <draifman@gmail.com>
2021-02-24 19:06:25 +0200

    input example

--- Lesson2/WebServer/static/js/App.js
+++ Lesson2/WebServer/static/js/App.js
@@ -1,6 +1,9 @@
 // App.js
 function click_me(){
     element = document.getElementById("response_id");
-    element.innerHTML = "Thank you very much for clicking"
+    element.innerHTML = "Thank you very much for clicking";
+//    num_elem = document.getElementById("number_field");
+//    text_elem = document.getElementById("text_field");
+//    text_elem.value = num_elem.value;
 }
 
