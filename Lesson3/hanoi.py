2047b89e8019de4c4a57b9ddba98204185732dda
Doron Raifman <draifman@gmail.com>
2021-03-01 18:50:37 +0200

    lesson3

--- Lesson3/hanoi.py
+++ Lesson3/hanoi.py
@@ -1,10 +1,10 @@
 def hanoi(n, src, use, dst):
     print(f'hanoi:n={n}, src:{src}, use:{use}, dst:{dst}')
     if n == 1:
-        print(f'--> move1:n={n}, move from src:{src} to dst:{dst}')
+        print(f'-> move1_1:n={n}, move from src:{src} to dst:{dst}')
     else:
         hanoi(n-1, src, dst, use)
-        print(f'--> move1:n={n}, move from src:{src} to dst:{dst}')
+        print(f'--> move1_2:n={n}, move from src:{src} to dst:{dst}')
         hanoi(n-1, use, dst, src)
 
 
