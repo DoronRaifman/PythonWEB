756fdaa64d736058d50b61f2d5d84abc96fe9b60
Doron Raifman <draifman@gmail.com>
2021-03-29 13:29:33 +0300

    some improvements

--- Lesson03/factorial.py
+++ Lesson03/factorial.py
@@ -1,5 +1,5 @@
 def factorial(n):
-    return(n * factorial(n-1) if n > 1 else 1)
+    return n * factorial(n-1) if n > 1 else 1
 
 
 n = 3
--- Lesson03/hanoi.py
+++ Lesson03/hanoi.py
@@ -1,13 +1,13 @@
 def hanoi(n, src, use, dst):
-    print(f'hanoi:n={n}, src:{src}, use:{use}, dst:{dst}')
+    # print(f'hanoi:n={n}, src:{src}, use:{use}, dst:{dst}')
     if n == 1:
-        print(f'-> move1_1:n={n}, move from src:{src} to dst:{dst}')
+        print(f'-> move1_1:n={n}, move from {src} to {dst}')
     else:
         hanoi(n-1, src, dst, use)
-        print(f'--> move1_2:n={n}, move from src:{src} to dst:{dst}')
+        print(f'-> move1_2:n={n}, move from {src} to {dst}')
         hanoi(n-1, use, dst, src)
 
 
 if __name__ == '__main__':
-    hanoi(2, 1, 2, 3)
+    hanoi(3, 'orig', 'help', 'dest')
 
