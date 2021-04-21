8299c32786c185c7d7a017200ae6c4296c920cc7
Doron Raifman <draifman@gmail.com>
2021-04-21 17:59:08 +0300

    added DB schema to lesson 8

--- Lesson07/Plotly/Plotly.py
+++ Lesson07/Plotly/Plotly.py
@@ -1,3 +1,4 @@
+import socket
 from flask import Flask, render_template, request, jsonify
 import json
 import numpy as np
@@ -12,7 +13,7 @@ def index():
 
 @app.route('/get_data', methods=['GET', 'POST'])
 def action_page_get():
-    length = 1000
+    length = 10000
     base_time = np.linspace(0.0, 5.0 * 2.0 * np.pi, length)
     noise = np.random.random(length)
     signal = np.round(10.0 * (np.sin(base_time) + 0.3 * noise))
@@ -22,5 +23,13 @@ def action_page_get():
 
 if __name__ == '__main__':
     print(f"start serving Site")
+    # host_name = socket.gethostname()
+    # real_ip = socket.gethostbyname(host_name)
+    # port = 25347
+    # url = f"http://{real_ip}:{port}"
+    # start browser
+    # threading.Timer(1.25, lambda: webbrowser.open(url)).start()
+    # print(f"start serving PurchaseList on url: {url}")
+    # app.run(host=real_ip, port=port)
     app.run()
 
